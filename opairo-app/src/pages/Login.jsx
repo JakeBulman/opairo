5db5fe68a3a9327624eec5c2f71ede7bcb01384d
Jake Bulman <jakebulman@hotmail.com>
2025-07-20 12:06:00 +0100

    Added Layout component to login and register pages

--- opairo-app/src/pages/Login.jsx
+++ opairo-app/src/pages/Login.jsx
@@ -1,28 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import LoginForm from "../components/authentication/LoginForm";
+import Layout from '../components/Layout';
 
 function Login() {
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-6 d-flex justify-content-center">
-                    <div className="content text-center p-5">
-                        <h1 className="text-primary pb-2">
-                            Welcome to Opairo!
-                        </h1>
-                        <p className="content">
-                            Please login to continue.
-                            <br />
-                            Don't have an account? <Link to="/register">Register</Link>
-                        </p>
+        <Layout>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6 d-flex justify-content-center">
+                        <div className="content text-center p-5">
+                            <h1 className="text-primary pb-2">
+                                Welcome to Opairo!
+                            </h1>
+                            <p className="content">
+                                Please login to continue.
+                                <br />
+                                Don't have an account? <Link to="/register">Register</Link>
+                            </p>
+                        </div>
+                    </div>
+                    <div className="col-md-6 p-5">
+                        <LoginForm />
                     </div>
                 </div>
-                <div className="col-md-6 p-5">
-                    <LoginForm />
-                </div>
             </div>
-        </div>
+        </Layout>
     );
 }
 
