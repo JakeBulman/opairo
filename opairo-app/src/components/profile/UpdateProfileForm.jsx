78ed967d99ab98625b7c8cf8305b753b92490b6e
Jake Bulman <jakebulman@hotmail.com>
2025-08-01 16:09:32 +0100

    Remove unused context import from UpdateProfileForm component

--- opairo-app/src/components/profile/UpdateProfileForm.jsx
+++ opairo-app/src/components/profile/UpdateProfileForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState } from 'react';
 import { Button, Form, Image } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 
