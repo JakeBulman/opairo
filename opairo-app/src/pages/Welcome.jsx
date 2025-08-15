8898389ad0b0a6d2cb39be7c2483c894f80b0df5
Jake Bulman <jakebulman@hotmail.com>
2025-08-16 00:51:22 +0100

    Add Welcome page and update navigation flow post-registration

---
+++ opairo-app/src/pages/Welcome.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Layout from '../components/Layout';
+
+function Welcome() {
+    return (
+        <Layout>
+            <div className="container">
+                <div className="row">
+
+                </div>
+            </div>
+        </Layout>
+    );
+}
+
+export default Welcome;
