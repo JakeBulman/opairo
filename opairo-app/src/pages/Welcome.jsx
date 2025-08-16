b63bd100c4889aedfbfb7bce746d93ae49a8d6f1
Jake Bulman <jakebulman@hotmail.com>
2025-08-16 01:10:46 +0100

    Refactor Welcome page to use profile card for user's own account

--- opairo-app/src/pages/Welcome.jsx
+++ opairo-app/src/pages/Welcome.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import Layout from '../components/Layout';
+import { getUser } from "../hooks/user.actions";
+import ProfileCard from '../components/profile/ProfileCard';
+import { Container, Row } from 'react-bootstrap';
 
 function Welcome() {
+
+    const account = getUser();
+
     return (
         <Layout>
-            <div className="container">
-                <div className="row">
-
-                </div>
-            </div>
+            <Container>
+                <Row className="p-4">
+                    <ProfileCard account={account} />
+                    <p className="p-4">This is your profile card, it's how everyone else at Opairo will see you. You might want to take some time now to personalise your profile.</p>
+                </Row>
+            </Container>
         </Layout>
     );
 }
