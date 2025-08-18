539b9b5bb3bb784b7b7927bd99fead5bc27a6ede
Jake Bulman <jakebulman@hotmail.com>
2025-08-18 15:58:33 +0100

    Refactor Welcome page to display profile card with fetched user data

--- opairo-app/src/pages/Welcome.jsx
+++ opairo-app/src/pages/Welcome.jsx
@@ -1,21 +1,26 @@
 import React from "react";
 import Layout from '../components/Layout';
 import { getUser } from "../hooks/user.actions";
+import useSWR from 'swr';
+import { fetcher } from '../helpers/axios';
 import ProfileCard from '../components/profile/ProfileCard';
-import { Container, Row } from 'react-bootstrap';
+import { Row, Card } from 'react-bootstrap';
 
 function Welcome() {
 
     const account = getUser();
+    const profile = useSWR(account ? `/account/${account.public_id}/` : null, fetcher).data;
 
     return (
         <Layout>
-            <Container>
-                <Row className="p-4">
-                    <ProfileCard account={account} />
-                    <p className="p-4">This is your profile card, it's how everyone else at Opairo will see you. You might want to take some time now to personalise your profile.</p>
-                </Row>
-            </Container>
+            <Row className="p-4">
+                <Card className="P-0">
+                <div className="mt-3">
+                <ProfileCard account={profile} />
+                </div>
+                <p className="p-3" style={{fontSize:12}}>This is your profile card, it's how everyone else at Opairo will see you. You might want to take some time now to personalise your profile.</p>
+                </Card>
+            </Row>
         </Layout>
     );
 }
