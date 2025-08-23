ad299d0bccd1e8573487298c889ad1fe6086c225
Jake Bulman <jakebulman@hotmail.com>
2025-08-23 16:18:05 +0100

    Add CreateEvent page and New Event button for user access

--- opairo-app/src/pages/Events.jsx
+++ opairo-app/src/pages/Events.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 import Layout from '../components/Layout';
-import { Row, Col, Card, Placeholder, Spinner } from 'react-bootstrap';
+import { Row, Col, Card, Button, Placeholder, Spinner } from 'react-bootstrap';
 import useSWR from 'swr'
 import EventCard from '../components/event/EventCard';
 import { fetcher } from '../helpers/axios';
+import { getUser } from '../hooks/user.actions';
+import { Link } from 'react-router-dom';
 
 
 function Home() {
-    const events = useSWR('/events/?limit=9', fetcher);
+    const events = useSWR('/event/?limit=9', fetcher);
+    const user = getUser();
+
     return (
         <Layout>
             <Row className='justify-content-evenly'>
                 <h4 className='text-center'>Events</h4>
+                { user.user_type === '2' ?
+                <div className="d-grid">
+                    <Button as={Link} to={`/events/create`} className="mt-3" variant="success" type="submit">
+                        New Event
+                    </Button>
+                </div>
+                : null }
                     {events.data ? (
                     events.data && events.data.results.map((event, index) => (
                     <Col className='p-4' xs={12} md={6} lg={4}>
