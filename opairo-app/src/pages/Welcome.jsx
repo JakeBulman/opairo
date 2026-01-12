02dce214b32ed9521dbaff413cbaf64d9f9b96dd
Jake Bulman <jakebulman@hotmail.com>
2026-01-12 01:29:12 +0000

    Add latest events section to Welcome component with loading placeholders

--- opairo-app/src/pages/Welcome.jsx
+++ opairo-app/src/pages/Welcome.jsx
@@ -5,13 +5,15 @@ import { getUser } from "../hooks/user.actions";
 import useSWR from 'swr';
 import { fetcher } from '../helpers/axios';
 import ProfileCard from '../components/profile/ProfileCard';
-import { Row, Card, Button } from 'react-bootstrap';
+import EventCard from '../components/event/EventCard';
+import { Row, Col, Card, Button, Placeholder, Spinner } from 'react-bootstrap';
 
 function Welcome() {
 
     const account = getUser();
     const profile = useSWR(account ? `/account/${account.public_id}/` : null, fetcher).data;
     const referrer_profile = useSWR(account ? `/account/${account.referrer}/` : null, fetcher).data;
+    const events = useSWR('/event/?limit=9', fetcher);
 
     return (
         <Layout>
@@ -40,6 +42,37 @@ function Welcome() {
                         </Button>
                     </>  
                     : null }
+                    <>    
+                        <Card.Header className="text-danger fw-bold pt-4">Latest Events</Card.Header>              
+                        <p className="px-3 pt-2" style={{fontSize:12}}>Check out some of the latest events happening on Opairo.</p>
+                                            {events.data ? (
+                        events.data && events.data.results.map((events, index) => (
+                            <div className='d-flex flex-column h-100'>
+                                <EventCard key={index} events={events} />
+                            </div>
+                            ))
+                        ) : (
+                            [...Array(9).keys()].map(key =>               
+                            <div className='d-flex flex-column'>
+                                <Card className="text-center" >
+                                    <Card.Header>
+                                        <Placeholder as={Card.Title} animation="wave">
+                                            <Placeholder xs={6} bg="secondary" />
+                                        </Placeholder>
+                                    </Card.Header>
+                                    <Card.Body >
+                                        <Spinner animation="border" variant="secondary" role="status" className="mb-3" style={{ width: '46px', height: '46px' }} />
+                                        <Placeholder as={Card.Text} animation="wave">
+                                            <Placeholder xs={4} bg="secondary" /> <Placeholder xs={6} bg="secondary" />{' '}
+                                        </Placeholder>
+                                        <Placeholder.Button xs={4} variant="primary" />
+                                    </Card.Body>
+                                </Card>
+                            </div>
+                            )
+                        )}
+                    </> 
+
                 </Card> 
             </Row>
             
